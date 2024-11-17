f6a21e5d1d1d11b4bd83fc68a79a3a127bfdc944
Foxies-CSTL <Foxies-CSTL@users.noreply.github.com>
2024-11-17 15:28:34 +0100

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3497,11 +3497,11 @@
    */
   #define HYBRID_THRESHOLD
 
-  #define X_HYBRID_THRESHOLD     150  // [mm/s]
+  #define X_HYBRID_THRESHOLD     250  // [mm/s]
   //#define X2_HYBRID_THRESHOLD    100
-  #define Y_HYBRID_THRESHOLD     150
+  #define Y_HYBRID_THRESHOLD     250
   //#define Y2_HYBRID_THRESHOLD    100
-  #define Z_HYBRID_THRESHOLD     150
+  #define Z_HYBRID_THRESHOLD     250
   //#define Z2_HYBRID_THRESHOLD      3
   //#define Z3_HYBRID_THRESHOLD      3
   //#define Z4_HYBRID_THRESHOLD      3
